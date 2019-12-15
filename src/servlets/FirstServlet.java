3309665cb39f386e1c7ff51273963435985f0c0b
Koshkarov Vitaliy <koshkarovvitaly123@gmail.com>
2019-12-15 15:21:27 +0300

    Creating session for user

--- src/servlets/FirstServlet.java
+++ src/servlets/FirstServlet.java
@@ -5,25 +5,36 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class FirstServlet extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
   }
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String name = request.getParameter("name");
-    String lastName = request.getParameter("lastName");
+    HttpSession session = request.getSession();
+    Integer count = (Integer) session.getAttribute("count");
+
+    if (count == null) {
+      count = 1;
+      session.setAttribute("count", 1);
+    } else {
+      session.setAttribute("count", count + 1);
+    }
+
+    //String name = request.getParameter("name");
+    //String lastName = request.getParameter("lastName");
 
     PrintWriter writer = response.getWriter();
 
     writer.println("<html>");
-    writer.println("<h2> Hello, " + name + " " + lastName + "! </h2>");
+    writer.println("<h2> Your count is: " + count + " </h2>");
+    //writer.println("<h2> Hello, " + name + " " + lastName + "! </h2>");
     writer.println("</html>");
-
-    response.sendRedirect("http://www.google.com");
   }
 }
